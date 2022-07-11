60d5729f14e3f5d380f3ae4f0eb59552a67c1f21
DevSottie <j.vitorsilva.dev@gmail.com>
2022-07-11 00:06:07 -0300

    swagger concluido

--- CalculaFreteAPI/src/main/java/com/sigabem/apiCalculaFrete/controller/CalculaFreteController.java
+++ CalculaFreteAPI/src/main/java/com/sigabem/apiCalculaFrete/controller/CalculaFreteController.java
@@ -7,7 +7,6 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,14 +20,16 @@ import com.sigabem.apiCalculaFrete.model.Cotacao;
 import com.sigabem.apiCalculaFrete.service.CalculaFreteService;
 import com.sigabem.apiCalculaFrete.service.ViaCepService;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @Controller
 @ResponseBody
 @RequestMapping("/api/v1/calculafrete")
-@Api(value = "API REST Calcula Frete")
-@CrossOrigin(origins="*")
+@Tag(name = "Cotações", description = "Endpoint que realiza a cotação do frete e prazo de entrega")
 public class CalculaFreteController {
 	
 	@Autowired
@@ -38,7 +39,18 @@ public class CalculaFreteController {
 	private ViaCepService viaCepService;
 	
 	@PostMapping
-	@ApiOperation(value = "Retorna o valor total do frete, data de entrega e ceps de origem e destino")
+	@Operation(
+			summary = "Faz a consulta do falor de frete e prazo de envio e salva no banco de dados",
+			tags = {"Cotações"},
+			responses = {
+					@ApiResponse(
+							description = "Success",
+							responseCode = "200",
+							content = @Content(mediaType = "application/json", schema = @Schema(implementation = CotacaoResponseDto.class))
+							),
+					@ApiResponse(description = "Internal error", responseCode = "500", content = @Content)
+				}
+			)
 	public ResponseEntity<CotacaoResponseDto> mostraCotacao(@RequestBody @Valid CotacaoRequestDto cotacaoRequest){
 		ViaCepResponseDto cepValidadoOrigem = viaCepService.consultaCep(cotacaoRequest.getCepOrigem());
 		ViaCepResponseDto cepValidadoDestino = viaCepService.consultaCep(cotacaoRequest.getCepDestino());
@@ -49,7 +61,18 @@ public class CalculaFreteController {
 	}
 	
 	@GetMapping
-	@ApiOperation(value = "Retorna a lista de todas as cotacoes já feitas e as datas em que foram consultadas")
+	@Operation(
+			summary = "Lista todas as cotações",
+			tags = {"Cotações"},
+			responses = {
+					@ApiResponse(
+							description = "Success",
+							responseCode = "200",
+							content = @Content(mediaType = "application/json", schema = @Schema(implementation = CotacaoResponseDto.class))
+							),
+					@ApiResponse(description = "Internal error", responseCode = "500", content = @Content)
+				}
+		)
 	public ResponseEntity<List<Cotacao>> buscaCotacoes(){
 		List<Cotacao> cotacoes = calculaFreteService.buscaCotacoes();
 		return ResponseEntity.ok().body(cotacoes);
